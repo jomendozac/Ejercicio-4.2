3b0118365e022552ed0ad41d0e23a5ecb9977234
jomendozac <141792489+jomendozac@users.noreply.github.com>
2025-01-30 17:30:44 -0500

    Create CasaUrbana.java

---
+++ CasaUrbana.java
@@ -0,0 +1,16 @@
+package Inmuebles;
+
+public class CasaUrbana extends Casa {
+    public CasaUrbana(int identificadorInmobiliario, int area, String direccion, int numeroHabitaciones, int numeroBaños, int numeroPisos) {
+
+        super(identificadorInmobiliario, area, direccion, numeroHabitaciones, numeroBaños, numeroPisos);
+    }
+
+    @Override
+    
+    public void imprimir() {
+
+        super.imprimir();
+    }
+    
+}
