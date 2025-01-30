1ce292deced2d16588fcb951a3c13ed91afad084
jomendozac <141792489+jomendozac@users.noreply.github.com>
2025-01-30 17:35:35 -0500

    Update InmuebleVivienda.java

--- InmuebleVivienda.java
+++ InmuebleVivienda.java
@@ -5,22 +5,18 @@ public class InmuebleVivienda extends Inmueble {
     protected int numeroHabitaciones;
     protected int numeroBaños;
 
-    // Constructor
     public Inmueble_vivienda(int identificadorInmobiliario, int area, String direccion, int numeroBaños, int numeroHabitaciones) {
 
-        // Llama al constructor de la superclase
         super(identificadorInmobiliario, area, direccion);
         
         this.numeroHabitaciones = numeroHabitaciones;
         this.numeroBaños = numeroBaños;
     }
 
-    // Método que muestra en pantalla los datos
-
     @Override
     public void imprimir() {
 
-        super.imprimir(); // Llama al método imprimir de la superclase
+        super.imprimir();
 
         System.out.println("Número de habitaciones = " + numeroHabitaciones);
         System.out.println("Número de baños = " + numeroBaños);
